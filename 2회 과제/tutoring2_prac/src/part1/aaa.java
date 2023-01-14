bfc1d49916800991bef7616ef71351c5b046d03d
tmddbs <ksy062700@naver.com>
2023-01-15 03:41:01 +0900

    2회 과제

---
+++ 2회 과제/tutoring2_prac/src/part1/aaa.java
@@ -0,0 +1,20 @@
+package part1;
+
+public class aaa extends bbb implements ccc{
+    int a;
+
+    public void ddd(){
+        System.out.println("111");
+    }
+    @Override
+    public void eee(){
+        System.out.println("222");
+    }
+    @Override
+    public void fff() {
+        System.out.println("333");
+    }
+
+
+}
+
---
+++ 2회 과제/tutoring2_prac/src/part1/bbb.java
@@ -0,0 +1,6 @@
+package part1;
+
+public abstract class bbb implements ccc{
+    public abstract void eee();
+
+}
