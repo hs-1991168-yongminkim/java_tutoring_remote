7968a47fbb2c3593e4514fdaffcdfb875458e3f2
tmddbs <ksy062700@naver.com>
2023-01-15 13:15:12 +0900

    2회

--- 2회 과제/tutoring2_prac/src/part1/bbb.java
+++
@@ -1,6 +0,0 @@
-package part1;
-
-public abstract class bbb implements ccc{
-    public abstract void eee();
-
-}
--- 2회 과제/tutoring2_prac/src/part1/ccc.java
+++
@@ -1,6 +0,0 @@
-package part1;
-
-public interface ccc {
-    public void fff();
-
-}
